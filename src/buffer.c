f81d5f88537ff3e49d306caa01b1ea7fff2d972b
Jack Kelly <jack@jackkelly.name>
2011-04-14 21:55:38 +1000

    Add struct buffer.

---
+++ src/buffer.c
@@ -0,0 +1,50 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#include "buffer.h"
+
+#include <string.h>
+
+#include "log.h"
+
+struct buffer* buffer_new(gint size) {
+  struct buffer* rv = g_malloc(sizeof(*rv) + size * sizeof(rv->data[0]));
+  rv->size = size;
+  rv->used = 0;
+  return rv;
+}
+
+void buffer_free(struct buffer* buffer) {
+  g_free(buffer);
+}
+
+gint buffer_append(struct buffer* buffer, const gchar* data, gint size) {
+  gint available = buffer->size - buffer->used;
+  if (size > available) {
+    memcpy(buffer->data + buffer->used, data, available);
+    buffer->used = buffer->size;
+    return available;
+  } else {
+    memcpy(buffer->data + buffer->used, data, size);
+    buffer->used += size;
+    return size;
+  }
+}
+
+gboolean buffer_append_c(struct buffer* buffer, gchar ch) {
+  if (buffer->size == buffer->used) return FALSE;
+  buffer->data[buffer->used++] = ch;
+  return TRUE;
+}
+
+void buffer_drain(struct buffer* buffer, gint size) {
+  if (size > buffer->used) {
+    ERROR("Draining more than currently in buffer! (%d > %d)",
+          size,
+          buffer->used);
+    buffer->used = 0;
+  } else {
+    buffer->used -= size;
+    memmove(buffer->data, buffer->data + size, buffer->used);
+  }
+}
---
+++ src/buffer.h
@@ -0,0 +1,83 @@
+/**
+ ** @node buffer.h
+ ** @section buffer.h
+ **/
+
+#ifndef BUFFER_H
+#define BUFFER_H
+
+#include <glib.h>
+
+/**
+ ** @deftp {Data Type} struct buffer
+ ** A dequeue-style buffer. Note that this is not a ring buffer, and
+ ** that calling @code{buffer_drain()} will @code{memmove(3)} the
+ ** remaining data in the buffer forwards.
+ **
+ ** @deftypeivar{struct buffer} gint size
+ ** Capacity of the buffer.
+ ** @end deftypeivar
+ ** @deftypeivar{struct buffer} gint used
+ ** Number of bytes currently used.
+ ** @end deftypeivar
+ ** @deftypeivar{struct buffer} gchar data[]
+ ** The buffered data.
+ ** @end deftypeivar
+ ** @end deftp
+ **/
+struct buffer {
+  gint size;
+  gint used;
+  gchar data[];
+};
+
+/**
+ ** @deftypefun {struct buffer*} buffer_new @
+ **   (gint @var{size})
+ ** Allocate and return a new buffer that can hold @var{size} bytes of
+ ** data. This function will never fail (it aborts on allocation
+ ** failure).
+ ** @end deftypefun
+ **/
+struct buffer* buffer_new(gint size);
+
+/**
+ ** @deftypefun void buffer_free @
+ **   (struct buffer* @var{buffer})
+ ** Free memory used by @var{buffer}.
+ ** @end deftypefun
+ **/
+void buffer_free(struct buffer* buffer);
+
+/**
+ ** @deftypefun gint buffer_append  @
+ **   (struct buffer* @var{buffer}, @
+ **    const gchar*   @var{data},   @
+ **    gint           @var{size})
+ ** Add @var{size} bytes from @var{data} to @var{buffer}. If the
+ ** buffer is too full, trailing data is not added. Returns the number
+ ** of bytes atually added.
+ ** @end deftypefun
+ **/
+gint buffer_append(struct buffer* buffer, const gchar* data, gint size);
+
+/**
+ ** @deftypefun gboolean buffer_append_c @
+ **   (struct buffer* @var{buffer},      @
+ **    gchar          @var{ch})
+ ** Append a single character to @var{buffer}. Return @code{TRUE} if
+ ** the character fit in the buffer.
+ ** @end deftypefun
+ **/
+gboolean buffer_append_c(struct buffer* buffer, gchar ch);
+
+/**
+ ** @deftypefun void buffer_drain   @
+ **   (struct buffer* @var{buffer}, @
+ **    gint           @var{size})
+ ** Remove the first @var{size} bytes from @var{buffer}.
+ ** @end deftypefun
+ **/
+void buffer_drain(struct buffer* buffer, gint size);
+
+#endif
